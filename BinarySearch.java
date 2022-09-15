04bb35178af5abec3e145a76e52b5185118989aa
IceCubeNext <107891941+IceCubeNext@users.noreply.github.com>
2022-09-16 01:08:58 +0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,1 +1,19 @@
+public class BinarySearch {
+    public int binarySearch(int[] container, int item) {
+        int result = -1;
+        int left = 0;
+        int right = container.length - 1;
 
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (container[mid] == item) {
+                return mid;
+            } else if (container[mid] < item) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
